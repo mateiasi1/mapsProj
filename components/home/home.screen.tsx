d8c5a867fc67709b607590488b1c497b929daada
Dragos Mateiasi <dragos.mateiasi@tokero.dev>
2022-07-20 14:50:49 +0300

    initial commit

---
+++ components/home/home.screen.tsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  FAB,
+  Button,
+  Paragraph,
+  Dialog,
+  Portal,
+  Provider,
+} from "react-native-paper";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCarCrash, faLock } from "@fortawesome/free-solid-svg-icons";
+
+type Prop = {
+  id: number;
+  latitude: number;
+  longitude: number;
+};
+
+const HomeScreen = () => {
+  const [markers, setMarkers] = useState<Prop[]>([
+    {
+      id: null,
+      latitude: null,
+      longitude: null,
+    },
+  ]);
+  const [visible, setVisible] = useState(false);
+  const onMapPress = (event) => {
+    const obj = {
+      id: event.nativeEvent.coordinate.latitude,
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+    } as Prop;
+    debugger;
+
+    if (markers.some((item) => item.id === obj.id)) {
+      setMarkers(markers.filter((item) => item.id !== obj.id));
+
+      return;
+    }
+    setMarkers([...markers, obj]);
+  };
+  const showDialog = () => setVisible(true);
+
+  const hideDialog = () => setVisible(false);
+  return (
+    <SafeAreaView style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        initialRegion={{
+          latitude: 44.32724051468231,
+          longitude: 23.800531066954136,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+        showsCompass={true}
+        showsBuildings={true}
+        showsTraffic={true}
+        showsIndoors={true}
+        zoomEnabled={true}
+        onPress={onMapPress}
+        //onPress={(event) => this.onMapPress(event)}
+      >
+        <Provider>
+          <View>
+            <Portal>
+              <Dialog visible={visible} onDismiss={hideDialog}>
+                <Dialog.Title>Alert</Dialog.Title>
+                <Dialog.Content>
+                  <Paragraph>This is simple dialog</Paragraph>
+                  <FontAwesomeIcon icon={faCarCrash} size={40} />
+                </Dialog.Content>
+                <Dialog.Actions>
+                  <Button onPress={hideDialog}>Done</Button>
+                </Dialog.Actions>
+              </Dialog>
+            </Portal>
+          </View>
+        </Provider>
+        {markers.map(
+          (marker) =>
+            marker.latitude !== null && (
+              <Marker
+                key={marker.id}
+                description="Origin"
+                coordinate={{
+                  latitude: marker.latitude,
+                  longitude: marker.longitude,
+                }}
+                tracksViewChanges={true}
+                // onDragEnd={(e) =>
+                //   setMarkers({
+                //     id: e.nativeEvent.coordinate.latitude,
+                //     latitude: e.nativeEvent.coordinate.latitude,
+                //     longitude: e.nativeEvent.coordinate.longitude,
+                //   })
+                // }
+              ></Marker>
+            )
+        )}
+      </MapView>
+
+      <FAB icon="plus" style={styles.fab} onPress={showDialog} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  fab: {
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    margin: 16,
+  },
+});
+export default HomeScreen;
